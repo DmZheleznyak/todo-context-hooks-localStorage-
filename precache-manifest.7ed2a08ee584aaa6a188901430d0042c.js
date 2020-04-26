c75214f956fce2f53e0e7c2a9d717df19383e058
DmZheleznyak <11lione1991@gmail.com>
2020-04-27 01:31:29 +0300

    Updates

---
+++ precache-manifest.7ed2a08ee584aaa6a188901430d0042c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64e481ad449c44fb29deedf0e997ac98",
+    "url": "/todo-context-hooks-localStorage-/index.html"
+  },
+  {
+    "revision": "16c6bdb8cf8da42a0a08",
+    "url": "/todo-context-hooks-localStorage-/static/css/main.f6cffc85.chunk.css"
+  },
+  {
+    "revision": "5c834e7e2086a73ddc7f",
+    "url": "/todo-context-hooks-localStorage-/static/js/2.4c38e796.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-context-hooks-localStorage-/static/js/2.4c38e796.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16c6bdb8cf8da42a0a08",
+    "url": "/todo-context-hooks-localStorage-/static/js/main.6ff860d4.chunk.js"
+  },
+  {
+    "revision": "d3ade79424293b2f80f3",
+    "url": "/todo-context-hooks-localStorage-/static/js/runtime-main.e1cb2581.js"
+  }
+]);
